dedbf5bb53b0260e204fad808d2633a23e713cf6
Cao Yen Ngoc <caoyenngoc299@gmail.com>
2017-04-29 22:05:33 +0700

    First commit

---
+++ FlickSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickSearch
+//
+//  Created by Anh Tuong Nguyen on 4/26/17.
+//  Copyright © 2017 Anh Tuong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
